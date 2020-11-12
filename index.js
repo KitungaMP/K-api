d9cbb6c8472e6ca8318945022bf0d063f4a0d436
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-11-13 00:53:39 +0200

    Post maison de vente

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
+const { mkdir } = require('fs')
 const app = express()
+
+var UUID = require("uuid");
+var BCrypt = require("bcryptjs");
 
 const MongoClient = require('mongodb').MongoClient
 
@@ -17,26 +21,67 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true})
 .then(client => {
   console.log('Connected to Database')
   const db = client.db('kmp-admin')
-  const quotesCollection = db.collection('one_plus')
+  const kmp_collection = db.collection('one_plus')
 
   app.post('/child', (req, res) => {
-    quotesCollection.insertOne(req.body)
+    kmp_collection.insertOne(req.body)
     .then(result => {
       console.log(result)
     })
     .catch(error => console.error(error))
   })
+
+  // Posts
   
+  // Post Maison_vente
+  
+  /*
+    mid
+    denomination
+    address
+    contact
+    geo { lat, long }
+    date_creation,
+    type ( Pharmacie , supermarket, ...)
+    etat
+
+  */
+
+  app.post("/maison_vente", (request, response) => {
+    if(!request.body.denomination){
+        return response.status(401).send({ "message": "Veiller completer la denomination de votre maison de vente"});
+    } else if(!request.body.address){
+        return response.status(401).send({ "message": "Veiller completer l'adresse de votre maison de vente"});
+    } else if(!request.body.type){
+        return response.status(401).send({ "message": "Veiller completer le type de votre maison de vente"});
+    }
+    
+    var maison_vente = {
+        "mid": UUID.v4(),
+        "denomination" : request.denomination,
+        "address" : request.address,
+        "contact" : request.contact,
+        "date_creation" : (new Date()).getTime(),
+        "type" : request.type,
+        "etat" : 0
+    }
+    kmp_collection.insertOne(maison_vente)
+    .then(result => {
+      console.log(result)
+      return response.send(group);
+
+    })
+    .catch(error => {
+      console.error(error)
+      return response.status(500).send(error);
+    })
+  });
+
 })
-.catch(error => console.error(error))
-
-
 
 app.post('/quotes', (req, res) => {
   console.log('Hellooooooooooooooooo!')
   console.log(req.body)
-  //return response.status(401).send({ "message": "Veillez vous identifier"});
-  //response.send({"pid":request.body.pid});
   res.send(req.body);
 })
 
