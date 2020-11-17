67b88952345d13dc879f5f8cd9ace990a5de216c
bvenceslas <bvenceslas@gmail.com>
2020-11-17 09:23:07 +0200

    Add longitude and latitude to Maison

--- models/maison.model.js
+++ models/maison.model.js
@@ -1,3 +1,4 @@
+const { Double } = require('mongodb');
 const mongoose = require('mongoose');
 const UUID = require("uuid");
 
@@ -7,11 +8,13 @@ const Maison = mongoose.Schema({
     phone: {type: String, required: [true, 'phone  ne doit pas être vide']},
     email: {type: String, required: [true, 'email  ne doit pas être vide']},
     type: {type: Number, default: 0},
+    longitude: {type: Double},
+    latitude: {type: Double},
     province: {type: String, required: [true, 'province ne doit pas être vide']},
     ville: {type: String, required: [true, 'ville  ne doit pas être vide']},
     quartier: {type: String, required: [true, 'quartier  ne doit pas être vide']},
-    avenue: {type: String, required: [true, 'avenue  ne doit pas être vide']},
-    numero: {type: Number, required: [true, 'avenue  ne doit pas être vide']},
+    avenue: {type: String},
+    numero: {type: Number},
     etat: {type: Number, default: 0},
     createAt: {type: Date, default: (new Date()).getTime()}
 });
