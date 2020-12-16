b99c79661b48254f25df1e1fed8345877b11cad1
bvenceslas <bvenceslas@gmail.com>
2020-12-16 14:25:25 +0200

    Add put card endpoint

--- routes/api.js
+++ routes/api.js
@@ -238,6 +238,14 @@ router.post('/cards', async (req, res) => {
         .catch(err => res.status(400).json({error_message:err}));
 });
 
+router.put('/cards/:cid', async (req, res) => {
+    const {id_card, uid, montant, date_expiration } = req.body;
+
+    Card.findByIdAndUpdate(req.params.cid, req.body)
+        .then(() => res.send('Votre carte a été mise à jour avec succès.'))
+        .catch(err => res.status(400).send({error_message: err}));
+})
+
 
 // PANIER ENDPOINTS
 
